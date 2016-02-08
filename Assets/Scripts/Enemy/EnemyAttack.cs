176da98ef0b853f8b6520c0385d8fc1513ed2632
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-02-08 00:34:32 +0000

    enemy attack scripts

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyAttack : MonoBehaviour {
+
+
+    public List<GameObject> enemiesInRange;
+
+    // Use this for initialization
+    void Start () {
+        enemiesInRange = new List<GameObject>();
+    }
+
+    // 1
+    void OnEnemyDestroy(GameObject enemy)
+    {
+        enemiesInRange.Remove(enemy);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // 2
+        if (other.gameObject.tag.Equals("Tower"))
+        {
+            enemiesInRange.Add(other.gameObject);
+            TowerDestructionDelegate del =
+                other.gameObject.GetComponent<TowerDestructionDelegate>();
+            del.enemyDelegate += OnEnemyDestroy;
+        }
+    }
+    // 3
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Tower"))
+        {
+            enemiesInRange.Remove(other.gameObject);
+            TowerDestructionDelegate del =
+                other.gameObject.GetComponent<TowerDestructionDelegate>();
+            del.enemyDelegate -= OnEnemyDestroy;
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/TowerDestructionDelegate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerDestructionDelegate : MonoBehaviour {
+
+    public delegate void EnemyDelegate(GameObject enemy);
+    public EnemyDelegate enemyDelegate;
+
+    void OnDestroy()
+    {
+        if (enemyDelegate != null)
+        {
+            enemyDelegate(gameObject);
+        }
+    }
+}
