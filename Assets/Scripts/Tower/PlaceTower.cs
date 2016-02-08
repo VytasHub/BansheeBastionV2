3efb124aa1c00f10d01fb2d8ab99e1f111f9e48e
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-02-08 23:07:18 +0000

    different type of towers implemented

---
+++ Assets/Scripts/Tower/PlaceTower.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceTower : MonoBehaviour {
+
+    public GameObject towerPrefab;
+    private GameObject tower;
+
+
+
+    private bool canPlaceTower()
+    {
+        return tower == null;
+    }
+
+    private bool canUpgradeMonster()
+    {
+        if (tower != null)
+        {
+            TowerData towerData = tower.GetComponent<TowerData>();
+            TowerLevel nextLevel = towerData.getNextLevel();
+            if (nextLevel != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void OnMouseUp()
+    {
+        if (canPlaceTower())
+        {
+            tower = (GameObject) Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+            // TODO: Deduct gold
+        }
+        else if (canUpgradeMonster())
+        {
+            tower.GetComponent<TowerData>().increaseLevel();
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+            // TODO: Deduct gold
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Tower/TowerData.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerData : MonoBehaviour {
+
+    public List<TowerLevel> levels;
+    private TowerLevel currentLevel;
+
+
+    void OnEnable()
+    {
+        CurrentLevel = levels[0];
+    }
+
+    public TowerLevel CurrentLevel
+    {
+        get
+        {
+            return currentLevel;
+        }
+        set
+        {
+            currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(currentLevel);
+
+            GameObject levelVisualization = levels[currentLevelIndex].visualization;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levelVisualization != null)
+                {
+                    if (i == currentLevelIndex)
+                    {
+                        levels[i].visualization.SetActive(true);
+                    }
+                    else {
+                        levels[i].visualization.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public TowerLevel getNextLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        int maxLevelIndex = levels.Count - 1;
+        if (currentLevelIndex < maxLevelIndex)
+        {
+            return levels[currentLevelIndex + 1];
+        }
+        else {
+            return null;
+        }
+    }
+
+    public void increaseLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        if (currentLevelIndex < levels.Count - 1)
+        {
+            CurrentLevel = levels[currentLevelIndex + 1];
+        }
+    }
+
+
+    
+}
+
+[System.Serializable]
+public class TowerLevel
+{
+    public int cost;
+    public GameObject visualization;
+}
